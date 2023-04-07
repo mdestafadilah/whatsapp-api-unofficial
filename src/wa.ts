3d670b8b0bf1fc498f516807f380416e11b1b7d8
mdestafadilah <desta.08b@gmail.com>
2023-04-08 04:31:11 +0700

    cleaning old code

--- src/wa.ts
+++ src/wa.ts
@@ -1,74 +1,48 @@
-import { Client, LocalAuth, LegacySessionAuth } from "whatsapp-web.js";
-import { generate } from "qrcode-terminal";
-import fs from 'fs';
+// Supports ES6
+import { create, Whatsapp } from '@wppconnect-team/wppconnect';
 
-type createSessionOptions = {
-	sessionId: string;
-};
-
-export async function createSession(options: createSessionOptions) {
-	const { sessionId } = options;
-
-	// client
-	const client = new Client({
-		authStrategy: new LocalAuth(),
-		puppeteer: {
-			ignoreDefaultArgs: ["--disable-extensions"],
-			args: ["--no-sandbox", "--disable-setuid-sandbox"],
-		},
-	});
-
-	// Generate
-	client.on("qr", (qr) => {
-		generate(qr, { small: true });
-	});
-
-	//teste if script is working. User send !ping e script return pong
-	client.on("message", (msg) => {
-		if (msg.body == "!ping") {
-			msg.reply("pong");
-		}
-	});
-
-	client.initialize();
-
-	//Closing correcily using CTRL+C
-	process.on("SIGINT", async () => {
-		console.log("(SIGINT) Shutting down...");
-		await client.destroy();
-		console.log("client destroyed");
-		process.exit(0);
-	});
-}
-
-const SESSION_FILE_PATH = './session.json';
-
-export async function listSessions() {
-
-    // Load the session data if it has been previously saved
-    let sessionData: any;
-    if(fs.existsSync(SESSION_FILE_PATH)) {
-        sessionData = require(SESSION_FILE_PATH);
+create({
+    session: 'sessionName', //Pass the name of the client you want to start the bot
+    catchQR: (base64Qrimg, asciiQR, attempts, urlCode) => {
+      console.log('Number of attempts to read the qrcode: ', attempts);
+      console.log('Terminal qrcode: ', asciiQR);
+      console.log('base64 image string qrcode: ', base64Qrimg);
+      console.log('urlCode (data-ref): ', urlCode);
+    },
+    statusFind: (statusSession, session) => {
+      console.log('Status Session: ', statusSession); //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken
+      //Create session wss return "serverClose" case server for close
+      console.log('Session name: ', session);
+    },
+    headless: true, // Headless chrome
+    devtools: false, // Open devtools by default
+    useChrome: true, // If false will use Chromium instance
+    debug: false, // Opens a debug session
+    logQR: true, // Logs QR automatically in terminal
+    browserWS: '', // If u want to use browserWSEndpoint
+    browserArgs: [''], // Parameters to be added into the chrome browser instance
+    puppeteerOptions: {}, // Will be passed to puppeteer.launch
+    disableWelcome: false, // Option to disable the welcoming message which appears in the beginning
+    updatesLog: true, // Logs info updates automatically in terminal
+    autoClose: 60000, // Automatically closes the wppconnect only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
+    tokenStore: 'file', // Define how work with tokens, that can be a custom interface
+    folderNameToken: './tokens', //folder name when saving tokens
+    // BrowserSessionToken
+    // To receive the client's token use the function await clinet.getSessionTokenBrowser()
+    sessionToken: {
+      WABrowserId: '"UnXjH....."',
+      WASecretBundle: '{"key":"+i/nRgWJ....","encKey":"kGdMR5t....","macKey":"+i/nRgW...."}',
+      WAToken1: '"0i8...."',
+      WAToken2: '"1@lPpzwC...."',
     }
-
-    const client = new Client({
-        authStrategy: new LegacySessionAuth({
-            session: sessionData // saved session object
-        })
-    });
-
-    // Save session values to the file upon successful auth
-    client.on('authenticated', (session) => {
-        sessionData = session;
-        fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
-            if (err) {
-                console.error(err);
-            }
-        });
-    });
-    
-}
+  })
+  .then((client) => start(client))
+  .catch((error) => console.log(error));
 
 export async function init() {
 	// TODO
 }
+function start(client: Whatsapp): any {
+	throw new Error('Function not implemented.');
+}
+
